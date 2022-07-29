11c87ca4763567888af0dd10eb71d3a875779ab8
Cristian Baez <106565940+Cristianpjc@users.noreply.github.com>
2022-07-28 20:04:03 -0400

    Tarea 1

---
+++ src/com/Tarea1/Tarea1.java
@@ -0,0 +1,32 @@
+package com.Tarea1;
+
+public class Tarea1 {
+    public static void main(String[] args) {
+
+        int a = 1;
+        int b = 2;
+        int c = 3;
+
+        System.out.println("Los numeros son: "+ a +","+ b +","+ c);
+
+        long cr = 23233;
+        System.out.println("Los numeros son: "+ cr);
+
+        double z = 12.2;
+        double x = 12.6;
+        System.out.println("Los numeros son:"+ z + ","+ z);
+         boolean verdadero = true;
+         boolean falso = false;
+        System.out.println (verdadero);
+        System.out.println (falso);
+
+        String[] nombres = {"cristian", "Selva", "Cati" };
+
+
+        for (int i = 0; i < nombres.length; i++) {
+            System.out.println(nombres[i]);
+        }
+
+
+    }
+}
